045c67b0295c1c1a2d8be316d0af7c07601594ca
Aik <ikogweassurance85@gmail.com>
2022-09-04 23:24:28 +0100

    I am done with the code for the hamburger menu, and I also added the required effects

--- src/aikscripts.js
+++ src/aikscripts.js
@@ -22,13 +22,15 @@ menu.addEventListener("click", () => {
     if(menu.getAttribute("src") === "./images/icon-hamburger.svg") {
         menu.setAttribute("src", "./images/icon-close.svg");
         mockups.style.display = "none";
-        mockups.parentElement.previousElementSibling.style.marginTop = "80%";
+        mockups.parentElement.previousElementSibling.style.paddingTop = "80%";
+        mockups.parentElement.previousElementSibling.style.background = "linear-gradient( hsla(233, 26%, 24%, 0.75),hsl(0, 0%, 100%))";
         hamburgerList.style.display = "flex";
     } else {
         menu.setAttribute("src", "./images/icon-hamburger.svg");
         hamburgerList.style.display = "none";
         mockups.style.display = "block";
-        mockups.parentElement.previousElementSibling.style.marginTop = "0%";
+        mockups.parentElement.previousElementSibling.style.paddingTop = "0%";
+        mockups.parentElement.previousElementSibling.style.background = "transparent";
     }  
 });
    
